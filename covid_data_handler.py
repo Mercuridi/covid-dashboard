9affe44f9d5f3e9729ad02d7b71a0567350f49f6
Mercuridi <95036629+Mercuridi@users.noreply.github.com>
2021-11-26 18:41:29 +0000

    adapted csv functions to work on json files

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -18,13 +18,6 @@ from uk_covid19 import Cov19API
 def parse_csv_data(csv_filename):
     covid_csv_data = []
     data = open(csv_filename, "r", encoding="UTF-8")
-    # this next (commented) line is to check if the data has been imported
-    # correctly by printing it to console; however, when
-    # run, it breaks the appending of the list csv_data
-    # current theory: data.read() changes the object type
-    # of data to a text wrapper, which has no actionable
-    # property "line"
-    #print (data.read() +("Raw data from input file"))
     for line in data:
         line = line.strip('\n')
         covid_csv_data.append(line)
@@ -116,7 +109,7 @@ def dictionary_combiner (local_data, national_data):
         national_data[i-1].update(local_data[i-1])
     all_data = national_data
     return all_data
-    
+
 def covid_API_request (location = "Exeter", location_type = "ltla"):
     # set variable for API queries
     location_info_local =       [
@@ -151,24 +144,89 @@ def covid_API_request (location = "Exeter", location_type = "ltla"):
     #print (data1, data2)
     # prepare and send useful data to be cached in json - 2 files
     """
-    # Commented out as 2 exports is unnecessary with the later method, 
+    # Commented out as 2 exports is unnecessary with the later method,
     # but keeping the code may be useful for debugging in the future
     export_file = open('data_national.json', 'w', encoding="UTF-8")
     json.dump(data_national, export_file, indent = "")
     export_file = open('data_local.json', 'w', encoding="UTF-8")
     json.dump(data_local, export_file, indent = "")
     """
-    
+
     data_all = dictionary_combiner(data_local, data_national)
 
     # export master dictionary to json file
     export_file = open('data_all.json', 'w', encoding="UTF-8")
     json.dump(data_all, export_file, indent = "")
-    
-    return (data_all)
 
-# run functions with given local copy of test data
+    return data_all
+
+def parse_json_data(json_filename = "data_all.json"):
+    with open(json_filename, "r", encoding ="UTF-8") as file:
+        covid_json_data = json.load(file)
+    return covid_json_data
+
+def process_covid_json_data(covid_json_data):
+    # define base state of variables
+    days_summed = 0
+    last7days_cases = 0
+
+    # set up latches
+    total_deaths_not_assigned = True
+    last7days_cases_not_complete = True
+    first_date_not_passed = True
+    current_hospital_cases_not_assigned = True
+
+    # loop through each dictionary of the given data until the return statement is triggered
+    for current_line in covid_json_data:
+
+        # process data by checking if data is valid to use
+        if current_line["cumDailyNsoDeathsByDeathDate"] != None and total_deaths_not_assigned:
+            # this if statement only assigns the first satisfactory value for deaths, as
+            # total_deaths is cumulative
+            total_deaths = current_line["cumDailyNsoDeathsByDeathDate"]
+            print (("Total deaths: ") + str(total_deaths))
+
+            # switch latch post assignment to prevent reassigning a wrong number in future loops
+            total_deaths_not_assigned = False
+
+        if current_line["hospitalCases"] != None and current_hospital_cases_not_assigned:
+            current_hospital_cases = current_line["hospitalCases"]
+            # switch latch post assignment to prevent reassigning a wrong number in future loops
+            current_hospital_cases_not_assigned = False
+            print (("Current hospital cases: ") + str(current_hospital_cases))
+
+        if current_line["newCasesBySpecimenDate"] != None and last7days_cases_not_complete:
+            if first_date_not_passed:
+                # switch latch if the first number (incomplete data) has just been read
+                first_date_not_passed = False
+
+            else:
+                # find new total cases and increment days
+                last7days_cases = last7days_cases + int(current_line["newCasesBySpecimenDate"])
+                days_summed += 1
+
+                #print (("Current running total for last 7 days cases: ") + str(last7days_cases))
+                # switch latch if data summed for the past week
+                if days_summed == 7:
+                    print (("Last 7 days cases: ") + str(last7days_cases))
+                    last7days_cases_not_complete = False
+
+        # check all tasks complete; if so, end function and return variables
+        # logic here checks if all 3 values are False ie. have the related variables been assigned
+        # checked in order of least likely to complete quickly to most likely to complete quickly
+        if not total_deaths_not_assigned and not current_hospital_cases_not_assigned and not last7days_cases_not_complete:
+            print ("All tasks in process_covid_json_data complete")
+            return(total_deaths, current_hospital_cases, last7days_cases)
+
+    # function should always complete the earlier check by the end of the given json
+    # if not, the for loop ends and this line will be printed
+    print ("Error: not all functions in process_covid_json_data completed")
+
+
+# run functions with local test data for CSV, live API call for JSON
 csv_data = parse_csv_data ("nation_2021-10-28.csv")
-deaths, hospital_cases, last_week_cases = process_covid_csv_data (csv_data)
-covid_API_request ()
 construct_csv_dictionary (parse_csv_data("nation_2021-10-28.csv"))
+process_covid_csv_data (csv_data)
+covid_API_request ()
+json_data = parse_json_data()
+deaths, hospital_cases, last_week_cases = process_covid_json_data (json_data)
