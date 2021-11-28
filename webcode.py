e5ce0efae802537a1756e5f687ac4b5e15c58c16
Mercuridi <95036629+Mercuridi@users.noreply.github.com>
2021-11-28 15:55:14 +0000

    restructure file system - deleted main.py

--- webcode.py
+++ webcode.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from covid_data_handler import *
+from covid_data_handler import process_covid_json_data
+from covid_data_handler import parse_json_data
+from covid_data_handler import covid_API_request
 
 app = Flask(__name__)
 
@@ -12,6 +14,6 @@ def website_update():
     local_7day_infections = process_covid_json_data (parse_json_data("data_local.json"))
     print (deaths_total, hospital_cases, national_7day_infections, local_7day_infections)
     return render_template("index.html")
-#local_7day_infections = local_7day_infections, national_7day_infections = national_7day_infections, deaths_total = deaths_total, hospital_cases = hospital_cases)
-if __name__ == '__main__':
-    app.run()
+#            local_7day_infections = local_7day_infections, national_7day_infections = national_7day_infections,
+#            deaths_total = deaths_total, hospital_cases = hospital_cases)
+app.run()
