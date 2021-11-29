165c9ef95aef3314c854bbed968e6ec3e3cf9456
Mercuridi <95036629+Mercuridi@users.noreply.github.com>
2021-11-29 16:50:53 +0000

    create news API request module

---
+++ covid_news_handling.py
@@ -0,0 +1,17 @@
+import requests
+import datetime
+
+def news_api_request (covid_terms = "Covid COVID-19 coronavirus"):
+    url = ('https://newsapi.org/v2/everything?'
+       f'q={covid_terms}&'
+       f'from={datetime.date}&'
+       'sortBy=popularity&'
+       'apiKey=d94f92a7e85348be8a233f2740d33a8f')
+
+    response = requests.get(url)
+    news_data = response.json()
+    print (response)
+    print (news_data)
+    
+news_api_request()
+    
